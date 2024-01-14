dc1777760e9b7006aa7aaa11a838316e3a50af91
arifur-rahman1 <aarifurrahman93@gmail.com>
2024-01-14 16:26:34 +0600

    added img in service

--- index.js
+++ index.js
@@ -42,9 +42,18 @@ async function run() {
       const id = req.params.id;
       const query={_id: new ObjectId(id)}
       const option ={
-        projection:{title:1,price:1,service_id:1},
+        projection:{title:1,price:1,service_id:1, img:1},
       }
       const result= await serviceCollection.findOne(query,option);
+      res.send(result);
+    })
+    // bookings
+    app.get('/bookings',async(req,res)=>{
+      let query={}
+      if(req.query?.email){
+        query={email:req.query.email}
+      }
+      const result=await bookingCollection.find(query).toArray();
       res.send(result);
     })
     app.post('/bookings',async(req,res)=>{
