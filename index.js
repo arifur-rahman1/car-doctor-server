f4371b96d611c7a2cb37f03b86c10c5170ecbdbf
arifur-rahman1 <aarifurrahman93@gmail.com>
2024-01-14 15:48:04 +0600

    booking data input done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app =express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -30,10 +30,27 @@ async function run() {
     await client.connect();
 
     const serviceCollection =client.db("carDoctor").collection('services');
+    const bookingCollection =client.db("carDoctor").collection('bookings');
 
     app.get('/services',async(req,res)=>{
       const cursor=serviceCollection.find();
       const result =await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/services/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query={_id: new ObjectId(id)}
+      const option ={
+        projection:{title:1,price:1,service_id:1},
+      }
+      const result= await serviceCollection.findOne(query,option);
+      res.send(result);
+    })
+    app.post('/bookings',async(req,res)=>{
+      const booking=req.body;
+      // console.log(booking);
+      const result =await bookingCollection.insertOne(booking);
       res.send(result);
     })
 
