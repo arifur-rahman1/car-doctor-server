fd1b98e9c250bc82d0cb5ad6b87271d9cd23a53c
arifur-rahman1 <aarifurrahman93@gmail.com>
2024-05-13 17:24:21 +0600

    token verified done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt =require('jsonwebtoken')
+const cookieParser=require('cookie-parser')
 const app =express();
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -16,6 +17,7 @@ app.use(cors({
   credentials:true
 }));
 app.use(express.json())
+app.use(cookieParser())
 
 
 
@@ -29,6 +31,28 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+// middlewares
+const logger =(req,res,next)=>{
+  console.log('log info',req.method, req.url);
+  next()
+}
+
+const verifyToken=(req,res,next)=>{
+  const token=req?.cookies?.token;
+  // console.log('token in the middleware',token);
+if(!token){
+  return res.status(401).send({message: "Unauthohrized Access"})
+}
+jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+  if(err){
+    return res.status(401).send({message: "Unauthohrized Access"})
+  }
+  req.user=decoded
+  next()
+})
+  // next()
+}
 
 async function run() {
   try {
@@ -54,7 +78,12 @@ async function run() {
       res.send(result);
     })
     // bookings
-    app.get('/bookings',async(req,res)=>{
+    app.get('/bookings',logger,verifyToken,async(req,res)=>{
+      console.log(req.query.email);
+      console.log('owner info', req.user);
+        if(req.user.email !== req.query.email){
+          return res.status(403).send({message: "forbidden access"})
+        }
       let query={}
       if(req.query?.email){
         query={email:req.query.email}
@@ -94,7 +123,7 @@ async function run() {
    
 
   //auth related api
-    app.post('/jwt',async(req,res)=>{
+    app.post('/jwt',logger,async(req,res)=>{
       const user = req.body;
       // console.log("user for token",user);
       const token =jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
